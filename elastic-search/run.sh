26abd5d48cfc158c08ec42b76082fb4c393e55bb
Bernhard Keprt <bernhard.keprt@goosefraba.at>
2016-04-15 14:22:14 +0200

    added restart always flag

--- elastic-search/run.sh
+++ elastic-search/run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -d -p 9200:9200 -p 9300:9300 -v $2:/data $1 /elasticsearch/bin/elasticsearch -Des.config=/elasticsearch/config/elasticsearch.yml
+docker run -d -p 9200:9200 -p 9300:9300 --restart=always -v $2:/data $1 /elasticsearch/bin/elasticsearch -Des.config=/elasticsearch/config/elasticsearch.yml
